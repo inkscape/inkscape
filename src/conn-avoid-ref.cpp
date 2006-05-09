597b246acebbf435b99cd31b65f9f7cbbcb70126
mjwybrow <mjwybrow@users.sourceforge.net>
2006-05-09 04:25:31 +0000

    790:       * conn-avoid-ref.cpp: Fix a crashbug that could occur in files         containing connectors if they were loaded from the file chooser         or from the recent file list.  Fixes bug #1458820.

--- src/conn-avoid-ref.cpp
+++ src/conn-avoid-ref.cpp
@@ -74,14 +74,24 @@ void SPAvoidRef::handleSettingChange(void)
     if (desktop == NULL) {
         return;
     }
+    if (sp_desktop_document(desktop) != item->document) {
+        // We don't want to go any further if the active desktop's document
+        // isn't the same as the document that this item is part of.  This
+        // case can happen if a new document is loaded from the file chooser
+        // or via the recent file menu.  In this case, we can end up here
+        // as a rersult of a sp_document_ensure_up_to_date performed on a
+        // document not yet attached to the active desktop.
+        return;
+    }
 
-    Router *router = item->document->router;
-    
     if (new_setting == setting) {
         // Don't need to make any changes
         return;
     }
+    setting = new_setting;
 
+    Router *router = item->document->router;
+    
     _transformed_connection.disconnect();
     if (new_setting) {
         _transformed_connection = item->connectTransformed(
@@ -110,7 +120,6 @@ void SPAvoidRef::handleSettingChange(void)
         router->delShape(shapeRef);
         shapeRef = NULL;
     }
-    setting = new_setting;
 }
 
 
