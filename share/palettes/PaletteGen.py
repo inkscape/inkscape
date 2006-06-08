7056e7bbf7918211fbc7b39205846b8527b97ee3
buliabyak <buliabyak@users.sourceforge.net>
2006-06-08 21:18:56 +0000

    1186:   fix truncated saturation, make l range variable within s

--- share/palettes/PaletteGen.py
+++ share/palettes/PaletteGen.py
@@ -20,28 +20,35 @@ for i in range(0, g_steps + 1):
     bval = int(round(b * 255))
     
     if i == 0:
-	line = "%3s %3s %3s  Black" % (rval, gval, bval)
+        line = "%3s %3s %3s  Black" % (rval, gval, bval)
     elif i == g_steps:
-	line = "%3s %3s %3s  White" % (rval, gval, bval)
+        line = "%3s %3s %3s  White" % (rval, gval, bval)
     else:
-	line = "%3s %3s %3s  %s%% Gray" % (rval, gval, bval, 100 - int(level * 100))
+        line = "%3s %3s %3s  %s%% Gray" % (rval, gval, bval, 100 - int(level * 100))
     print line
 
-    # add two more steps near white
+    # add three more steps near white
     if i == g_steps - 1:
-	level_m = level + 0.5 * g_step_size
-	r, g, b = colorsys.hls_to_rgb(0, level_m, 0)
-	rval = int(round(r * 255))
-	gval = int(round(g * 255))
-	bval = int(round(b * 255))
-	print "%3s %3s %3s  %s%% Gray" % (rval, gval, bval, 100 - int(level_m * 100))
+        level_m = level + 0.25 * g_step_size
+        r, g, b = colorsys.hls_to_rgb(0, level_m, 0)
+        rval = int(round(r * 255))
+        gval = int(round(g * 255))
+        bval = int(round(b * 255))
+        print "%3s %3s %3s  %s%% Gray" % (rval, gval, bval, 100 - (level_m * 100))
 
-	level_mm = level + 0.75 * g_step_size
-	r, g, b = colorsys.hls_to_rgb(0, level_mm, 0)
-	rval = int(round(r * 255))
-	gval = int(round(g * 255))
-	bval = int(round(b * 255))
-	print "%3s %3s %3s  %s%% Gray" % (rval, gval, bval, 100 - (level_mm * 100))
+        level_m = level + 0.5 * g_step_size
+        r, g, b = colorsys.hls_to_rgb(0, level_m, 0)
+        rval = int(round(r * 255))
+        gval = int(round(g * 255))
+        bval = int(round(b * 255))
+        print "%3s %3s %3s  %s%% Gray" % (rval, gval, bval, 100 - int(level_m * 100))
+
+        level_mm = level + 0.75 * g_step_size
+        r, g, b = colorsys.hls_to_rgb(0, level_mm, 0)
+        rval = int(round(r * 255))
+        gval = int(round(g * 255))
+        bval = int(round(b * 255))
+        print "%3s %3s %3s  %s%% Gray" % (rval, gval, bval, 100 - (level_mm * 100))
 
 
 # standard HTML colors
@@ -60,15 +67,16 @@ print '''128   0   0  Maroon (#800000)
 
 # HSL palette
 h_steps = 15
-s_steps = 4
-l_steps = 11
+s_steps = 3
+l_steps = 14
 h_step_size = 1.0 / h_steps
 s_step_size = 1.0 / s_steps
-l_step_size = 1.0 / l_steps
 
 for h in range(0, h_steps):
-    for s in range(1, s_steps):
-        for l in range(1, l_steps):
+    for s in range(0, s_steps):
+        l_range = int(round(l_steps - (s*6/s_steps))) - 2
+        l_step_size = 1.0 / l_range
+        for l in range(1, l_range):
             hval = h * h_step_size
             sval = 1 - (s * s_step_size)
             lval = l * l_step_size
