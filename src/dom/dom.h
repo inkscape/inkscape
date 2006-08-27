c3f1c7b04c4a9771447a5b5275740d71d1a01490
ishmal <ishmal@users.sourceforge.net>
2006-08-27 14:45:12 +0000

    1643:   domexception enums should be class scoped

--- src/dom/dom.h
+++ src/dom/dom.h
@@ -141,6 +141,32 @@ class DOMException
 
 public:
 
+    /**
+     * ExceptionCode
+     */
+    typedef enum
+        {
+        INDEX_SIZE_ERR                 = 1,
+        DOMSTRING_SIZE_ERR             = 2,
+        HIERARCHY_REQUEST_ERR          = 3,
+        WRONG_DOCUMENT_ERR             = 4,
+        INVALID_CHARACTER_ERR          = 5,
+        NO_DATA_ALLOWED_ERR            = 6,
+        NO_MODIFICATION_ALLOWED_ERR    = 7,
+        NOT_FOUND_ERR                  = 8,
+        NOT_SUPPORTED_ERR              = 9,
+        INUSE_ATTRIBUTE_ERR            = 10,
+        INVALID_STATE_ERR              = 11,
+        SYNTAX_ERR                     = 12,
+        INVALID_MODIFICATION_ERR       = 13,
+        NAMESPACE_ERR                  = 14,
+        INVALID_ACCESS_ERR             = 15,
+        VALIDATION_ERR                 = 16,
+        TYPE_MISMATCH_ERR              = 17
+        } ExceptionCode;
+
+
+
     DOMException(const DOMString &reasonMsg)
         { msg = reasonMsg; }
 
@@ -176,29 +202,6 @@ public:
 
 
 
-/**
- * ExceptionCode
- */
-typedef enum
-{
-        INDEX_SIZE_ERR                 = 1,
-        DOMSTRING_SIZE_ERR             = 2,
-        HIERARCHY_REQUEST_ERR          = 3,
-        WRONG_DOCUMENT_ERR             = 4,
-        INVALID_CHARACTER_ERR          = 5,
-        NO_DATA_ALLOWED_ERR            = 6,
-        NO_MODIFICATION_ALLOWED_ERR    = 7,
-        NOT_FOUND_ERR                  = 8,
-        NOT_SUPPORTED_ERR              = 9,
-        INUSE_ATTRIBUTE_ERR            = 10,
-        INVALID_STATE_ERR              = 11,
-        SYNTAX_ERR                     = 12,
-        INVALID_MODIFICATION_ERR       = 13,
-        NAMESPACE_ERR                  = 14,
-        INVALID_ACCESS_ERR             = 15,
-        VALIDATION_ERR                 = 16,
-        TYPE_MISMATCH_ERR              = 17
-} ExceptionCode;
 
 
 /*#########################################################################
