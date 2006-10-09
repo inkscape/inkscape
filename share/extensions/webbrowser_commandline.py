308b699c3d37b35a6ae9b4352c26dd4d505a6c48
buliabyak <buliabyak@users.sourceforge.net>
2006-10-09 23:54:04 +0000

    1791:   more help commands

---
+++ share/extensions/webbrowser_commandline.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python 
+import webbrowser
+webbrowser.open("http://inkscape.org/doc/inkscape-man.html")
---
+++ share/extensions/webbrowser_faq.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python 
+import webbrowser
+webbrowser.open("http://wiki.inkscape.org/wiki/index.php/FAQ")
