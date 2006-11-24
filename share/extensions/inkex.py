8f29015171d3f31c7a005b93761d31bdf4e4ce21
acspike <acspike@users.sourceforge.net>
2006-11-24 19:01:55 +0000

    2029:   forgot re

--- share/extensions/inkex.py
+++ share/extensions/inkex.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
-import sys, copy, optparse, random
+import sys, copy, optparse, random, re
 
 #a dictionary of all of the xmlns prefixes in a standard inkscape doc
 NSS = {
