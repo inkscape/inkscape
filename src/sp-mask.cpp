5e6cc1a4deba17ad8a64501c24e9f254ad9d86d2
buliabyak <buliabyak@users.sourceforge.net>
2007-03-03 06:33:55 +0000

    2516:   fix warning when creating a mask which is the first item in defs

--- src/sp-mask.cpp
+++ src/sp-mask.cpp
@@ -293,7 +293,8 @@ sp_mask_create (GSList *reprs, SPDocument *document, NR::Matrix const* applyTran
         }
     }
 
-    defsrepr->changeOrder(repr, defsrepr->lastChild()); // workaround for bug 989084
+    if (repr != defsrepr->lastChild())
+        defsrepr->changeOrder(repr, defsrepr->lastChild()); // workaround for bug 989084
     
     Inkscape::GC::release(repr);
     return mask_id;
