72e8a814387f2f006f458f1e73c6a375e514f9df
gouldtj <gouldtj@users.sourceforge.net>
2007-07-03 04:13:31 +0000

    3165:    r15736@tres:  ted | 2007-07-02 21:11:39 -0700    Changing the working directory for scripts from the temporary directory    to the current working directory (the one Inkscape was called with).  I    don't think this is the 'right' way to do this -- but the win32 people    kinda need it.

--- src/extension/implementation/script.cpp
+++ src/extension/implementation/script.cpp
@@ -914,7 +914,7 @@ Script::execute (const std::list<std::string> &in_command,
     int stdout_pipe, stderr_pipe;
 
     try {
-        Glib::spawn_async_with_pipes(Glib::get_tmp_dir(), // working directory
+        Glib::spawn_async_with_pipes(Glib::get_current_dir(), // working directory
                                      argv,  // arg v
                                      Glib::SPAWN_SEARCH_PATH /*| Glib::SPAWN_DO_NOT_REAP_CHILD*/,
                                      sigc::slot<void>(),
