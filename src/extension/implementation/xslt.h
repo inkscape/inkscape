32e4ee0d49153df4b709a61c32f5aae844c78418
gouldtj <gouldtj@users.sourceforge.net>
2007-09-12 06:36:45 +0000

    3729:    r16525@tres:  ted | 2007-09-11 20:49:47 -0700    Adding in code to XSLT.

--- src/extension/implementation/xslt.h
+++ src/extension/implementation/xslt.h
@@ -4,7 +4,7 @@
  * Authors:
  *   Ted Gould <ted@gould.cx>
  *
- * Copyright (C) 2006 Authors
+ * Copyright (C) 2006-2007 Authors
  *
  * Released under GNU GPL, read the file 'COPYING' for more information
  */
@@ -13,6 +13,10 @@
 #define __INKSCAPE_EXTENSION_IMPEMENTATION_XSLT_H__
 
 #include "implementation.h"
+
+#include "libxml/tree.h"
+#include "libxslt/xslt.h"
+#include "libxslt/xsltInternals.h"
 
 namespace Inkscape {
 namespace XML {
@@ -27,10 +31,22 @@ namespace Implementation {
 
 class XSLT : public Implementation {
 private:
+    std::string _filename;
+    xmlDocPtr _parsedDoc;
+    xsltStylesheetPtr _stylesheet;
 
+    Glib::ustring solve_reldir(Inkscape::XML::Node *reprin);
 public:
     XSLT (void);
 
+    bool load(Inkscape::Extension::Extension *module);
+    void unload(Inkscape::Extension::Extension *module);
+
+    bool check(Inkscape::Extension::Extension *module);
+
+    SPDocument *open(Inkscape::Extension::Input *module,
+                     gchar const *filename);
+    void save(Inkscape::Extension::Output *module, SPDocument *doc, gchar const *filename);
 };
 
 }  /* Inkscape  */
