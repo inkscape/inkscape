60f14a6ce58236f072e31d71afe3093671fe7e6f
johanengelen <johanengelen@users.sourceforge.net>
2007-11-01 21:58:50 +0000

    4007:   change mingwenv.bat according to new devlibs dir

--- mingwenv.bat
+++ mingwenv.bat
@@ -1,5 +1,5 @@
 @echo Setting environment variables for MinGw build of Inkscape
-set GTK=c:/gtk210
-set GTKDOS=c:\gtk210
+set GTK=c:/devlibs
+set GTKDOS=c:\devlibs
 set path=c:\mingw\bin;%GTKDOS%\bin;%path%
 
