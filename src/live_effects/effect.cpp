c20823075b5438ff6f14bea1bb90d23247b3a6a2
joelholdsworth <joelholdsworth@users.sourceforge.net>
2008-01-04 23:52:06 +0000

    4386:   Corrected tooltips and labels in the LPEs

--- src/live_effects/effect.cpp
+++ src/live_effects/effect.cpp
@@ -48,13 +48,13 @@ namespace LivePathEffect {
 const Util::EnumData<EffectType> LPETypeData[INVALID_LPE] = {
     // {constant defined in effect.h, N_("name of your effect"), "name of your effect in SVG"}
     {PATH_ALONG_PATH,       N_("Bend Path"),             "bend_path"},
-    {SKELETAL_STROKES,      N_("Pattern along path"),    "skeletal"},
+    {SKELETAL_STROKES,      N_("Pattern Along Path"),    "skeletal"},
 #ifdef LPE_ENABLE_TEST_EFFECTS
     {SLANT,                 N_("Slant"),                 "slant"},
     {DOEFFECTSTACK_TEST,    N_("doEffect stack test"),   "doeffectstacktest"},
 #endif
     {GEARS,                 N_("Gears"),                 "gears"},
-    {CURVE_STITCH,          N_("Stitch subpaths"),       "curvestitching"},
+    {CURVE_STITCH,          N_("Stitch Sub-Paths"),       "curvestitching"},
 };
 const Util::EnumDataConverter<EffectType> LPETypeConverter(LPETypeData, INVALID_LPE);
 
--- src/live_effects/lpe-curvestitch.cpp
+++ src/live_effects/lpe-curvestitch.cpp
@@ -41,9 +41,9 @@ using namespace Geom;
 LPECurveStitch::LPECurveStitch(LivePathEffectObject *lpeobject) :
     Effect(lpeobject),
     strokepath(_("Stroke path"), _("The path that will be used as stitch."), "strokepath", &wr, this, "M0,0 L1,0"),
-    nrofpaths(_("Nr of paths"), _("The number of paths that will be generated."), "count", &wr, this, 5),
-    startpoint_variation(_("Startpoint variation"), _("..."), "startpoint_variation", &wr, this, 0),
-    endpoint_variation(_("Endpoint variation"), _("..."), "endpoint_variation", &wr, this, 0),
+    nrofpaths(_("Number of paths"), _("The number of paths that will be generated."), "count", &wr, this, 5),
+    startpoint_variation(_("Start point jitter"), _("The amount of random jitter to apply to the start points of the stitches"), "startpoint_variation", &wr, this, 0),
+    endpoint_variation(_("End point jitter"), _("The amount of random jitter to apply to the end points of the stitches"), "endpoint_variation", &wr, this, 0),
     spacing_variation(_("Spacing variation"), _("Determines whether lines cluster together or have an equal spacing between each other."), "spacing_variation", &wr, this, 0),
     prop_scale(_("Scale width"), _("Scaling of the width of the stroke path"), "prop_scale", &wr, this, 1),
     scale_y_rel(_("Scale width relative"), _("Scale the width of the stroke path relative to its length"), "scale_y_rel", &wr, this, false)
