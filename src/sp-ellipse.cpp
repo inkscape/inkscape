3e2822e327be6f5aab2d5611f3ebdc7b8a129ea2
tavmjong <tavmjong@users.sourceforge.net>
2008-02-04 12:47:40 +0000

    4650:   Remove g_print debugging statement.

--- src/sp-ellipse.cpp
+++ src/sp-ellipse.cpp
@@ -427,7 +427,6 @@ sp_ellipse_init(SPEllipse */*ellipse*/)
 static void
 sp_ellipse_build(SPObject *object, SPDocument *document, Inkscape::XML::Node *repr)
 {
-    g_print ("sp_ellipse_build\n");
     if (((SPObjectClass *) ellipse_parent_class)->build)
         (* ((SPObjectClass *) ellipse_parent_class)->build) (object, document, repr);
 
