e6c433ebff8b4db646809c474e296512a6765723
ishmal <ishmal@users.sourceforge.net>
2008-02-07 08:58:13 +0000

    4673:   Changed by request to GPL2

--- src/dom/odf/odfdocument.cpp
+++ src/dom/odf/odfdocument.cpp
@@ -9,7 +9,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -22,6 +22,8 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * For more information, please write to rwjj@earthlink.net
+ *
+ *  RWJ :  080207: Changed to GPL2 by me
  *
  */
 
