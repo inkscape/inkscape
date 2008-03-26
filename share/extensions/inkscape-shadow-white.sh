859522c7794a8f019e5b17a07017b1c898737608
buliabyak <buliabyak@users.sourceforge.net>
2008-03-26 06:09:34 +0000

    5196:   remove the inkscape-shadow hack as no longer needed

--- share/extensions/inkscape-shadow-white.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-convert -mattecolor "#ffff" -frame $((${2} * 3))x$((${2} * 3)) -fx '1' -channel A -blur $((${2} * 3))x${2} $1 $1
--- share/extensions/inkscape-shadow.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-convert -mattecolor "#000f" -frame $((${2} * 3))x$((${2} * 3)) -fx '0' -channel A -blur $((${2} * 3))x${2} $1 $1
