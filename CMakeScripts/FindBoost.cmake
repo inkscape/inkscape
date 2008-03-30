b32b9705256fa42488f72cd2576de73a956c18db
johanengelen <johanengelen@users.sourceforge.net>
2008-03-30 19:17:06 +0000

    5255:   clean up FindBoost.cmake

--- CMakeScripts/FindBoost.cmake
+++ CMakeScripts/FindBoost.cmake
@@ -1,7 +1,7 @@
 # - Find Boost libraries
 # Go hunting for boost compoments
 # Defines:
-#  BOOST_INCLUDE
+#  BOOST_INCLUDE_DIR
 
 
 # To find boost on Windows, use BOOST_PATH variable set by mingwenv.bat:
@@ -13,33 +13,14 @@ FIND_PATH(BOOST_INCLUDE_DIR boost/weak_ptr.hpp
                             env_boost_path )
 
 
-IF(MINGW)
-  SET (BOOST_ROOT env_boost_path)
-  FIND_LIBRARY( BOOST_PYTHON_LIBRARY 
-                libboost_python-mgw
-                PATHS ${BOOST_ROOT}\\lib )
-  FIND_LIBRARY( BOOST_PYTHON_LIBRARY_DEBUG 
-                libboost_python-mgw-d
-                PATHS ${BOOST_ROOT}\\lib )
-ELSE(MINGW)
-  FIND_LIBRARY( BOOST_PYTHON_LIBRARY NAMES boost_python
-                PATHS /usr/lib /usr/local/lib ${BOOST_ROOT}\\lib )
-  FIND_LIBRARY( BOOST_PYTHON_LIBRARY_DEBUG NAMES boost_python-d
-                PATHS /usr/lib /usr/local/lib ${BOOST_ROOT}\\lib )
-ENDIF(MINGW)
-
 IF (BOOST_INCLUDE_DIR)
   SET(BOOST_FOUND TRUE)
 ENDIF (BOOST_INCLUDE_DIR)
 
 IF (BOOST_FOUND)
-  IF (NOT Boost_FIND_QUIETLY)
      MESSAGE(STATUS "Found Boost: ${BOOST_INCLUDE_DIR}")
-  ENDIF (NOT Boost_FIND_QUIETLY)
 ELSE(BOOST_FOUND)
-  IF (Boost_FIND_REQUIRED)
      MESSAGE(FATAL_ERROR "Could not find Boost")
-  ENDIF (Boost_FIND_REQUIRED)
 ENDIF (BOOST_FOUND)
 
 
