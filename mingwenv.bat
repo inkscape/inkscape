ca190ab301fe1e8d19b488c30b389ebbe36cff54
johanengelen <johanengelen@users.sourceforge.net>
2008-03-30 19:07:04 +0000

    5252:   add boost path to mingwenv.bat

--- mingwenv.bat
+++ mingwenv.bat
@@ -3,5 +3,6 @@ set MINGW_BIN=c:\mingw\bin
 set CMAKE_BIN=c:\cmake\bin
 set GTK=c:/devlibs
 set GTKDOS=c:\devlibs
-set PKG_CONFIG_PATH=c:\devlibs\lib\pkgconfig
+set PKG_CONFIG_PATH=c:/devlibs/lib/pkgconfig
+set BOOST_PATH=c:/devlibs/boost
 set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTKDOS%\bin;%PATH%
