02a86ae52fd5aa5bd9766e035aa1fe688828edbb
johanengelen <johanengelen@users.sourceforge.net>
2008-04-07 20:12:57 +0000

    5381:   cmake: add \\ to string define so result is something like:  -DSSL=\" sfsdf \"    (note the slashes in front of the ")

--- CMakeScripts/ConfigCompileFlags.cmake
+++ CMakeScripts/ConfigCompileFlags.cmake
@@ -19,7 +19,7 @@ ELSE(WIN32)
 ENDIF (WIN32)
 
 # for Inkboard:
-SET(DEFINE_FLAGS "${DEFINE_FLAGS} -DHAVE_SSL -DRELAYTOOL_SSL=\"static const int libssl_is_present=1; static int __attribute__((unused)) libssl_symbol_is_present(char *s){ return 1; }\" ")
+SET(DEFINE_FLAGS "${DEFINE_FLAGS} -DHAVE_SSL -DRELAYTOOL_SSL=\\\"static const int libssl_is_present=1; static int __attribute__((unused)) libssl_symbol_is_present(char *s){ return 1; }\\\" ")
 
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMPILE_WARNING_FLAGS} ${COMPILE_OPTIMIZATION_FLAGS} ${DEFINE_FLAGS} ${COMPILE_PROFILING_FLAGS} ")
 SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMPILE_WARNING_FLAGS} ${COMPILE_OPTIMIZATION_FLAGS} ${DEFINE_FLAGS} ${COMPILE_PROFILING_FLAGS} ")
