8e272128e2deea4b07456e13407edc87c7789c32
verbalshadow <verbalshadow@users.sourceforge.net>
2008-04-07 03:53:51 +0000

    5366:   Cmake: Moved CMAKE_C_FLAGS to ConfigCompileFlags.cmake

--- CMakeScripts/ConfigCompileFlags.cmake
+++ CMakeScripts/ConfigCompileFlags.cmake
@@ -1,3 +1,4 @@
+SET(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS}")
 SET(COMPILE_WARNING_FLAGS "-Wall -Wformat-security -W -Wpointer-arith -Wcast-align -Wsign-compare -Woverloaded-virtual -Wswitch")
 SET(COMPILE_OPTIMIZATION_FLAGS "-O2")
 
