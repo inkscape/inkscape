078784454ea56880988fbce75e13d963465603c1
buliabyak <buliabyak@users.sourceforge.net>
2008-06-14 04:12:54 +0000

    5924:   patch by sas from 227449 with my changes for using ps2pdf instead of pstoedit

--- share/extensions/uniconv-ext.py
+++ share/extensions/uniconv-ext.py
@@ -19,54 +19,12 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-
 """
 
-import os
-import random
-import subprocess
 import sys
-import tempfile
+from run_command import run
 
-# We need a filename ending in ".svg" for UniConvertor.
-# This is a hack.
-chars = list("0123456789abcdefghijklmnopqrstuvwxyz")
-while True:
-    random.shuffle(chars)
-    svgfile = os.path.join(tempfile.gettempdir(), ''.join(chars) + '.svg')
-    if not os.path.exists(svgfile):
-        break
-
-# Run UniConvertor, and determine our return code.
-try:
-    p = subprocess.Popen('uniconv "%s" "%s"' % (sys.argv[1], svgfile),
-                         shell=True, stderr=subprocess.PIPE)
-    err = p.stderr.read()
-    rc = p.wait()
-    if rc:
-        sys.stderr.write("UniConvertor failed: %s\n" % err)
-        rc = 1
-except Exception, inst:
-    sys.stderr.write("Spawn error: %s\n" % str(inst))
-    rc = 1
-
-# If successful, copy the SVG file to stdout.
-if rc == 0:
-    try:
-        f = open(svgfile, "rU")
-        for line in f:
-            sys.stdout.write(line)
-        f.close()
-    except IOError, inst:
-        sys.stderr.write("Error reading temporary SVG file: %s\n" % str(inst))
-        rc = 1
-
-# Clean up and return.
-try:
-    os.remove(svgfile)
-except Exception:
-    pass
-sys.exit(rc)
+run('uniconv "%s" "%%s"' % sys.argv[1], "UniConvertor")
 
 
 # vim: expandtab shiftwidth=4 tabstop=8 softtabstop=4 encoding=utf-8 textwidth=99
