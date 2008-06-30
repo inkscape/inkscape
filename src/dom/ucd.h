2e683aa922acb8b2a23f4ada5488d962c06d1fe1
ishmal <ishmal@users.sourceforge.net>
2008-06-30 20:04:11 +0000

    6103:   fix range problem

--- src/dom/ucd.h
+++ src/dom/ucd.h
@@ -1,17 +1,25 @@
 /**
+ * Phoebe DOM Implementation.
  *
- * Inkscape Unicode Character Database (UCD) 5.1.0. Utility 
+ * This is a C++ approximation of the W3C DOM model, which follows
+ * fairly closely the specifications in the various .idl files, copies of
+ * which are provided for reference.  Most important is this one:
  *
+ * http://www.w3.org/TR/2004/REC-DOM-Level-3-Core-20040407/idl-definitions.html
+ * 
+ * More thorough explanations of the various classes and their algorithms
+ * can be found there.
+ *     
  *
  * Authors:
  *   Bob Jamison
  *
- * Copyright (C) 2008 Bob Jamison
+ * Copyright (C) 2006-2008 Bob Jamison
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
- *  version 3 of the License, or (at your option) any later version.
+ *  version 2.1 of the License, or (at your option) any later version.
  *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,7 +29,7 @@
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ *  
  */
 #ifndef __UCD_H__
 #define __UCD_H__
