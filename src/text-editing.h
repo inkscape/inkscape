cb9a7db90cb1047ea2a2975d3c2658a0a085688d
pjrm <pjrm@users.sourceforge.net>
2008-07-05 12:04:22 +0000

    6188:   text-editing.h: Supply missing #includes/declarations so that we don't depend on order of .h file inclusion.  (Detected by check-header-compile.)  (Using separate commits to facilitate diagnosis/resolution of any problems; comments welcome.)

--- src/text-editing.h
+++ src/text-editing.h
@@ -14,8 +14,13 @@
  */
 
 #include <glib/gtypes.h>
-struct SPItem;
+#include <utility>   // std::pair
+#include "libnrtype/Layout-TNG.h"
 class SPCSSAttr;
+struct SPItem;
+struct SPObject;
+struct SPStyle;
+namespace NR { class Matrix; }
 namespace NR { class Point; }
 
 typedef std::pair<Inkscape::Text::Layout::iterator, Inkscape::Text::Layout::iterator> iterator_pair; 
