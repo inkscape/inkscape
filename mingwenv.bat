6b36e69bc8bd785db730d79540872ab405b76028
ishmal <ishmal@users.sourceforge.net>
2008-07-18 14:55:51 +0000

    6351:   Add python for cxxtests

--- mingwenv.bat
+++ mingwenv.bat
@@ -5,4 +5,4 @@ set GTK=c:/devlibs
 set GTKDOS=c:\devlibs
 set PKG_CONFIG_PATH=c:/devlibs/lib/pkgconfig
 set DEVLIBS_PATH=c:/devlibs
-set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTKDOS%\bin;%PATH%
+set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTKDOS%\bin;%GTKDOS%\python;%PATH%
