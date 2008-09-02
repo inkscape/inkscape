fe343f109e92753b15b0f71e3f0b5a0d7c937e0e
johanengelen <johanengelen@users.sourceforge.net>
2008-09-02 23:21:31 +0000

    6757:   add lpe param: VectorParam<float> and VectorParam<double>

---
+++ src/live_effects/parameter/vector.h
@@ -0,0 +1,123 @@
+#ifndef INKSCAPE_LIVEPATHEFFECT_PARAMETER_VECTOR_H
+#define INKSCAPE_LIVEPATHEFFECT_PARAMETER_VECTOR_H
+
+/*
+ * Inkscape::LivePathEffectParameters
+ *
+* Copyright (C) Johan Engelen 2008 <j.b.c.engelen@utwente.nl>
+ *
+ * Released under GNU GPL, read the file 'COPYING' for more information
+ */
+
+#include <vector>
+
+#include <glib/gtypes.h>
+
+#include <gtkmm/tooltips.h>
+
+#include "live_effects/parameter/parameter.h"
+
+#include "svg/svg.h"
+#include "svg/stringstream.h"
+
+namespace Inkscape {
+
+namespace LivePathEffect {
+
+template <typename StorageType>
+class VectorParam : public Parameter {
+public:
+    VectorParam( const Glib::ustring& label,
+                const Glib::ustring& tip,
+                const Glib::ustring& key,
+                Inkscape::UI::Widget::Registry* wr,
+                Effect* effect,
+                size_t n = 0 )
+        : Parameter(label, tip, key, wr, effect), _vector(n)
+    {
+
+    }
+
+    virtual ~VectorParam() {
+
+    };
+
+    std::vector<StorageType> const & data() const {
+        return _vector;
+    }
+
+    virtual Gtk::Widget * param_newWidget(Gtk::Tooltips * /*tooltips*/) {
+        return NULL;
+    }
+
+    virtual bool param_readSVGValue(const gchar * strvalue) {
+        _vector.clear();
+        gchar ** strarray = g_strsplit(strvalue, "|", 0);
+        gchar ** iter = strarray;
+        while (*iter != NULL) {
+            _vector.push_back( readsvg(*iter) );
+            iter++;
+        }
+        g_strfreev (strarray);
+        return true;
+    }
+
+    virtual gchar * param_getSVGValue() const {
+        Inkscape::SVGOStringStream os;
+        writesvg(os, _vector);
+        gchar * str = g_strdup(os.str().c_str());
+        return str;
+    }
+
+    void param_setValue(std::vector<StorageType> const &new_vector) {
+        _vector = new_vector;
+    }
+
+    void param_set_default() {
+        param_setValue( std::vector<StorageType>() );
+    }
+
+    void param_set_and_write_new_value(std::vector<StorageType> const &new_vector) {
+        Inkscape::SVGOStringStream os;
+        writesvg(os, new_vector);
+        gchar * str = g_strdup(os.str().c_str());
+        param_write_to_repr(str);
+        g_free(str);
+    }
+
+private:
+    VectorParam(const VectorParam&);
+    VectorParam& operator=(const VectorParam&);
+
+    std::vector<StorageType> _vector;
+
+    void writesvg(SVGOStringStream &str, std::vector<StorageType> const &vector) const {
+        for (unsigned int i = 0; i < vector.size(); ++i) {
+            if (i != 0) {
+                // separate items with pipe symbol
+                str << " | ";
+            }
+            str << vector[i];
+        }
+    }
+
+    StorageType readsvg(const gchar * str);
+};
+
+
+} //namespace LivePathEffect
+
+} //namespace Inkscape
+
+#endif
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
