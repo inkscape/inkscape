4dd1c81dfc3e51650bb9b1cac7e6367e190ea88b
tweenk <tweenk@users.sourceforge.net>
2008-09-16 22:09:44 +0000

    6828:   Add a header with forward declarations for the XML namespace.

---
+++ src/xml/xml-forward.h
@@ -0,0 +1,57 @@
+#ifndef __SEEN_XML_FORWARD_H__
+#define __SEEN_XML_FORWARD_H__
+
+/** @file
+ * @brief Forward declarations for the XML namespace.
+ */
+/* Authors:
+ *   Krzysztof Kosiński <tweenk.pl@gmail.com>
+ *
+ * Copyright (C) 2008 Authors
+ *
+ * Released under GNU GPL.  Read the file 'COPYING' for more information.
+ */
+
+namespace Inkscape {
+namespace XML {
+
+/* Copied from the relevant Doxygen page */
+
+struct AttributeRecord;
+struct CommentNode;
+class  CompositeNodeObserver;
+struct Document;
+class  ElementNode;
+class  Event;
+class  EventAdd;
+class  EventDel;
+class  EventChgAttr;
+class  EventChgContent;
+class  EventChgOrder;
+class  InvalidOperationException;
+class  LogBuilder;
+struct NodeEventVector;
+struct NodeSiblingIteratorStrategy;
+struct NodeParentIteratorStrategy;
+class  NodeObserver;
+class  Node;
+class  SimpleDocument;
+class  SimpleNode;
+class  Subtree;
+struct TextNode;
+
+} // namespace XML
+} // namespace Inkscape
+
+#endif // __SEEN_XML_FORWARD_H__
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
