6aeb3fd4d4c6bfa14b3b976447d819cd68c79f16
ishmal <ishmal@users.sourceforge.net>
2008-09-19 17:06:35 +0000

    6846:   Change function arg from NR::Matrix to Geom::Matrix.   Possible serious bug.

--- src/display/canvas-text.cpp
+++ src/display/canvas-text.cpp
@@ -32,7 +32,7 @@ static void sp_canvastext_class_init (SPCanvasTextClass *klass);
 static void sp_canvastext_init (SPCanvasText *canvastext);
 static void sp_canvastext_destroy (GtkObject *object);
 
-static void sp_canvastext_update (SPCanvasItem *item, NR::Matrix const &affine, unsigned int flags);
+static void sp_canvastext_update (SPCanvasItem *item, Geom::Matrix const &affine, unsigned int flags);
 static void sp_canvastext_render (SPCanvasItem *item, SPCanvasBuf *buf);
 
 static SPCanvasItemClass *parent_class_ct;
@@ -128,7 +128,7 @@ sp_canvastext_render (SPCanvasItem *item, SPCanvasBuf *buf)
 }
 
 static void
-sp_canvastext_update (SPCanvasItem *item, NR::Matrix const &affine, unsigned int flags)
+sp_canvastext_update (SPCanvasItem *item, Geom::Matrix const &affine, unsigned int flags)
 {
     SPCanvasText *cl = SP_CANVASTEXT (item);
 
