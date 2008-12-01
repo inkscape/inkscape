c9330f85695000f71fe68a0b0aeca9d1bd0491e8
aurium <aurium@users.sourceforge.net>
2008-12-01 22:41:45 +0000

    6929:   correcting the changes to provide compatibility for uniconversor name variations

--- share/extensions/uniconv-ext.py
+++ share/extensions/uniconv-ext.py
@@ -25,7 +25,7 @@ import sys
 from run_command import run
 from subprocess import Popen, PIPE
 
-#sys.stderr.write('aaaaaaa')
+cmd = 'none'
 
 try:
     p = Popen('uniconv', shell=True, stdout=PIPE, stderr=PIPE).wait()
@@ -33,18 +33,18 @@ try:
     p = Popen('uniconvertor', shell=True, stdout=PIPE, stderr=PIPE).wait()
     if p!=127 : cmd = 'uniconvertor'
 except ImportError:
-    try:
-        from popen2 import Popen3
-        p = Popen3('uniconv', True).wait()
-        if p!=32512 : cmd = 'uniconv'
-        p = Popen3('uniconvertor', True).wait()
-        if p!=32512 : cmd = 'uniconvertor'
-    except ImportError:
-        sys.stderr.write('You need to install the UniConvertor software.\n'+\
-                         'For Linux: install the packge python-uniconvertor.\n'+\
-                         'For Windows: download it from\n'+\
-                         'http://sk1project.org/modules.php?name=Products&product=uniconvertor\n')
+    from popen2 import Popen3
+    p = Popen3('uniconv', True).wait()
+    if p!=32512 : cmd = 'uniconv'
+    p = Popen3('uniconvertor', True).wait()
+    if p!=32512 : cmd = 'uniconvertor'
 
-run((cmd+' "%s" "%%s"') % sys.argv[1].replace("%","%%"), "UniConvertor")
+if cmd == 'none' :
+    sys.stderr.write('You need to install the UniConvertor software.\n'+\
+                     'For Linux: install the packge python-uniconvertor.\n'+\
+                     'For Windows: download it from\n'+\
+                     'http://sk1project.org/modules.php?name=Products&product=uniconvertor\n')
+else :
+    run((cmd+' "%s" "%%s"') % sys.argv[1].replace("%","%%"), "UniConvertor")
 
 # vim: expandtab shiftwidth=4 tabstop=8 softtabstop=4 encoding=utf-8 textwidth=99
