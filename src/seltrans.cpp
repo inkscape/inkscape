91f8ff9f466bc474d70a14ec0086d7a56d5bfaf0
buliabyak <buliabyak@users.sourceforge.net>
2009-10-04 15:20:53 +0000

    8709:   Diederik's patch for crash bug 441255

--- src/seltrans.cpp
+++ src/seltrans.cpp
@@ -408,8 +408,9 @@ void Inkscape::SelTrans::grab(Geom::Point const &p, gdouble x, gdouble y, bool s
         // Optionally, show the snap source
         if (!(_state == STATE_ROTATE && x != 0.5 && y != 0.5)) { // but not when we're dragging a rotation handle, because that won't snap
             // Now either _bbox_points or _snap_points has a single element, the other one has zero..... or both have zero elements
-            g_assert((_snap_points.size() + _bbox_points.size() + _bbox_points_for_translating.size()) == 1);
-            if (m.snapprefs.getSnapEnabledGlobally()) {
+            if ((_snap_points.size() + _bbox_points.size() + _bbox_points_for_translating.size()) > 1) {
+                g_warning("too many snap sources to display, please fix this");
+            } else if (m.snapprefs.getSnapEnabledGlobally()) {
                 if (_bbox_points.size() == 1) {
                     _desktop->snapindicator->set_new_snapsource(_bbox_points.at(0));
                 } else if (_bbox_points_for_translating.size() == 1) {
