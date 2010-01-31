f25b23d094f3b004fc3949820f12655130f6faea
Krzysztof Kosiński <tweenk.pl@gmail.com>
2010-01-31 03:54:43 +0100

    9036:   Tweak build.xml DLL names to match upgraded devlibs

--- mingwenv.bat
+++ mingwenv.bat
@@ -1,8 +1,6 @@
 @echo Setting environment variables for MinGw build of Inkscape
-set MINGW_BIN=c:\mingw\bin
-set CMAKE_BIN=c:\cmake\bin
-set GTK=c:/devlibs
-set GTKDOS=c:\devlibs
-set PKG_CONFIG_PATH=c:/devlibs/lib/pkgconfig
-set DEVLIBS_PATH=c:/devlibs
-set PATH=%MINGW_BIN%;%CMAKE_BIN%;%GTKDOS%\bin;%GTKDOS%\python;%PATH%
+IF "%DEVLIBS_PATH%"=="" set DEVLIBS_PATH=c:\devlibs
+IF "%MINGW_PATH%"=="" set MINGW_PATH=c:\mingw
+set MINGW_BIN=%MINGW_PATH%\bin
+set PKG_CONFIG_PATH=%DEVLIBS_PATH%\lib\pkgconfig
+set PATH=%MINGW_BIN%;%DEVLIBS_PATH%\bin;%DEVLIBS_PATH%\python;%PATH%
