c63e4901f48f1eb016f62980dbe303253b071b9a
Michael Wybrow <Michael.Wybrow@infotech.monash.edu.au>
2010-03-18 15:53:50 +1100

    9206:   Use gtk-engines2 rather than the obsolete gtk2-clearlooks package when   building Inkscape.app for Mac OS X. ------------------------------------------------------------ Use --include-merged or -n0 to see merged revisions.

--- packaging/macosx/osx-app.sh
+++ packaging/macosx/osx-app.sh
@@ -153,13 +153,13 @@ if [ ! -e "$LIBPREFIX" ]; then
 	exit 1
 fi
 
-if [ ! -e "$LIBPREFIX/share/themes/Clearlooks-Quicksilver" ]; then
-	echo "Missing Clearlooks -- please install gtk2-clearlooks and try again." >&2
+if ! pkg-config --exists gtk-engines-2; then
+	echo "Missing gtk-engines2 -- please install gtk-engines2 and try again." >&2
 	exit 1
 fi
 
-if [ ! -e "$LIBPREFIX/lib/gnome-vfs-2.0" ]; then
-	echo "Missing gnome-vfs -- please install gnome-vfs and try again." >&2
+if ! pkg-config --exists gnome-vfs-2.0; then
+	echo "Missing gnome-vfs2 -- please install gnome-vfs2 and try again." >&2
 	exit 1
 fi
 
