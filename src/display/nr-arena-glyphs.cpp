dd3e09fcd1984ac1cf2fb72284828c55cf34275b
buliabyak <buliabyak@users.sourceforge.net>
2010-03-28 00:15:37 -0300

    9245:   reinstate wrongly ignored delta in pick; makes selecting small text objects much easier

--- src/display/nr-arena-glyphs.cpp
+++ src/display/nr-arena-glyphs.cpp
@@ -234,7 +234,7 @@ nr_arena_glyphs_clip(NRArenaItem *item, NRRectL */*area*/, NRPixBlock */*pb*/)
 }
 
 static NRArenaItem *
-nr_arena_glyphs_pick(NRArenaItem *item, Geom::Point p, gdouble /*delta*/, unsigned int /*sticky*/)
+nr_arena_glyphs_pick(NRArenaItem *item, Geom::Point p, gdouble delta, unsigned int /*sticky*/)
 {
     NRArenaGlyphs *glyphs;
 
@@ -246,7 +246,7 @@ nr_arena_glyphs_pick(NRArenaItem *item, Geom::Point p, gdouble /*delta*/, unsign
     double const x = p[Geom::X];
     double const y = p[Geom::Y];
     /* With text we take a simple approach: pick if the point is in a characher bbox */
-    if ((x >= item->bbox.x0) && (y >= item->bbox.y0) && (x <= item->bbox.x1) && (y <= item->bbox.y1)) return item;
+    if ((x + delta >= item->bbox.x0) && (y + delta >= item->bbox.y0) && (x - delta <= item->bbox.x1) && (y - delta <= item->bbox.y1)) return item;
 
     return NULL;
 }
