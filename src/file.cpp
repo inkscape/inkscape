45d449186a8b54498daef52babcd09d5b22c7edd
Krzysztof Kosiński <tweenk.pl@gmail.com>
2010-07-17 00:29:25 +0200

    9618:   Fix build failure when DBus is not enabled

--- src/file.cpp
+++ src/file.cpp
@@ -42,7 +42,6 @@
 #include "extension/input.h"
 #include "extension/output.h"
 #include "extension/system.h"
-#include "extension/dbus/dbus-init.h"
 #include "file.h"
 #include "helper/png-write.h"
 #include "id-clash.h"
@@ -67,6 +66,10 @@
 
 #ifdef WITH_GNOME_VFS
 # include <libgnomevfs/gnome-vfs.h>
+#endif
+
+#ifdef WITH_DBUS
+#include "extension/dbus/dbus-init.h"
 #endif
 
 //#ifdef WITH_INKBOARD
@@ -136,7 +139,11 @@ sp_file_new(const Glib::ustring &templ)
         sp_namedview_window_from_document(dt);
         sp_namedview_update_layers_from_document(dt);
     }
+
+#ifdef WITH_DBUS
     Inkscape::Extension::Dbus::dbus_init_desktop_interface(dt);
+#endif
+
     return dt;
 }
 
