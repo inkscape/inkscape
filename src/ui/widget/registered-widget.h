b89f6c58963bc6fed36b358899e6302b71e0e732
Jon A. Cruz <jon@joncruz.org>
2011-06-25 15:47:10 -0700

    10360:   Removed questionable cast.

--- src/ui/widget/registered-widget.h
+++ src/ui/widget/registered-widget.h
@@ -62,8 +62,7 @@ public:
 
     bool is_updating() {if (_wr) return _wr->isUpdating(); else return false;}
 
-    // provide automatic 'upcast' for ease of use. (do it 'dynamic_cast' instead of 'static' because who knows what W is)
-    operator const Gtk::Widget () { return dynamic_cast<Gtk::Widget*>(this); }
+    operator const Gtk::Widget() { return *this; }
 
 protected:
     RegisteredWidget() : W() { construct(); }
