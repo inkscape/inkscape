6f1b789332d3eadf1663c221e2c6076e49064735
Gellule Xg <gellule.xg@gmail.com>
2011-06-28 14:50:06 -1000

    10381:   If GTK is built with the QUARTZ backend, there is no need to expect DISPLAY to be set to start the GUI version. ------------------------------------------------------------ Use --include-merged or -n0 to see merged revisions.

--- src/main.cpp
+++ src/main.cpp
@@ -641,7 +641,7 @@ main(int argc, char **argv)
 
     gboolean use_gui;
 
-#ifndef WIN32
+#if !defined(WIN32) && !defined(GDK_WINDOWING_QUARTZ)
     use_gui = (g_getenv("DISPLAY") != NULL);
 #else
     use_gui = TRUE;
