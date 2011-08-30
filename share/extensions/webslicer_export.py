c8eff961a064d60d240bdefb3b23e6d7cea2473c
JazzyNico <nicoduf@yahoo.fr>
2011-08-30 22:14:06 +0200

    10601:   i18n. Fix for bug #745285 (bad English translations in inkscape).

--- share/extensions/webslicer_export.py
+++ share/extensions/webslicer_export.py
@@ -54,8 +54,8 @@ class WebSlicer_Export(WebSlicer_Effect):
     def validate_inputs(self):
         # The user must supply a directory to export:
         if is_empty( self.options.dir ):
-            inkex.errormsg(_('You must to give a directory to export the slices.'))
-            return {'error':'You must to give a directory to export the slices.'}
+            inkex.errormsg(_('You must give a directory to export the slices.'))
+            return {'error':'You must give a directory to export the slices.'}
         # No directory separator at the path end:
         if self.options.dir[-1] == '/' or self.options.dir[-1] == '\\':
             self.options.dir = self.options.dir[0:-1]
