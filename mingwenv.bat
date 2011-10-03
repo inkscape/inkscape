eaae8be2cd1dcfd1a9b4da72ecd1b3b5b63775ea
Johan Engelen <goejendaagh@zonnet.nl>
2011-10-04 00:14:35 +0200

    10661:   add ghostscript to path for doxygen

--- mingwenv.bat
+++ mingwenv.bat
@@ -3,4 +3,5 @@ IF "%DEVLIBS_PATH%"=="" set DEVLIBS_PATH=c:\devlibs
 IF "%MINGW_PATH%"=="" set MINGW_PATH=c:\mingw
 set MINGW_BIN=%MINGW_PATH%\bin
 set PKG_CONFIG_PATH=%DEVLIBS_PATH%\lib\pkgconfig
-set PATH=%DEVLIBS_PATH%\bin;%DEVLIBS_PATH%\python;%MINGW_BIN%;%PATH%
+set GS_BIN=C:\latex\gs\gs8.61\bin
+set PATH=%DEVLIBS_PATH%\bin;%DEVLIBS_PATH%\python;%MINGW_BIN%;%PATH%;%GS_BIN%
