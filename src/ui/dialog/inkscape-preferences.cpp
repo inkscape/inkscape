5a7595d3213189dc35aa5c66c58cec9767c1af64
Jon A. Cruz <jon@joncruz.org>
2012-04-21 21:31:59 -0700

    11277:   Correcting message for lcms2 status. Fixes bug #985623.

--- src/ui/dialog/inkscape-preferences.cpp
+++ src/ui/dialog/inkscape-preferences.cpp
@@ -851,10 +851,10 @@ void InkscapePreferences::initPageIO()
     Glib::ustring intentLabels[numIntents] = {_("Perceptual"), _("Relative Colorimetric"), _("Saturation"), _("Absolute Colorimetric")};
     int intentValues[numIntents] = {0, 1, 2, 3};
 
-#if !defined(HAVE_LIBLCMS1) || defined(HAVE_LIBLCMS2)
+#if !defined(HAVE_LIBLCMS1) && !defined(HAVE_LIBLCMS2)
     Gtk::Label* lbl = new Gtk::Label(_("(Note: Color management has been disabled in this build)"));
     _page_cms.add_line( false, "", *lbl, "", "", true);
-#endif // !defined(HAVE_LIBLCMS1) || defined(HAVE_LIBLCMS2)
+#endif // !defined(HAVE_LIBLCMS1) && !defined(HAVE_LIBLCMS2)
 
     _page_cms.add_group_header( _("Display adjustment"));
 
@@ -913,6 +913,8 @@ void InkscapePreferences::initPageIO()
                         _("Enables black point compensation"), false);
 
     _cms_proof_preserveblack.init( _("Preserve black"), "/options/softproof/preserveblack", false);
+
+#if !defined(HAVE_LIBLCMS2)
     _page_cms.add_line( true, "", _cms_proof_preserveblack,
 #if defined(cmsFLAGS_PRESERVEBLACK)
                         "",
@@ -920,6 +922,7 @@ void InkscapePreferences::initPageIO()
                         _("(LittleCMS 1.15 or later required)"),
 #endif // defined(cmsFLAGS_PRESERVEBLACK)
                         _("Preserve K channel in CMYK -> CMYK transforms"), false);
+#endif // !defined(HAVE_LIBLCMS2)
 
 #if !defined(cmsFLAGS_PRESERVEBLACK)
     _cms_proof_preserveblack.set_sensitive( false );
