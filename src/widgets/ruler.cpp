a4632e45fcf4e4052decd4fd13bb07a36ae9c71d
Alex Valavanis <valavanisalex@gmail.com>
2012-05-06 17:57:41 +0100

    11334:   Don't set colormap in ruler

--- src/widgets/ruler.cpp
+++ src/widgets/ruler.cpp
@@ -452,13 +452,12 @@ sp_ruler_realize (GtkWidget *widget)
   attributes.height = allocation.height;
   attributes.wclass = GDK_INPUT_OUTPUT;
   attributes.visual = gtk_widget_get_visual (widget);
-  attributes.colormap = gtk_widget_get_colormap (widget);
   attributes.event_mask = gtk_widget_get_events (widget);
   attributes.event_mask |= (GDK_EXPOSURE_MASK |
 			    GDK_POINTER_MOTION_MASK |
 			    GDK_POINTER_MOTION_HINT_MASK);
 
-  attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL | GDK_WA_COLORMAP;
+  attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL;
 
   window = gdk_window_new(gtk_widget_get_parent_window (widget), 
 		          &attributes, attributes_mask);
