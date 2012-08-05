08bdc9c91aac26cd1d491570e6fdfc7daba9f6d9
Kris <Kris.De.Gussem@hotmail.com>
2012-08-05 09:53:07 +0200

    11593:   object attributes code was actually completely rewritten

--- src/ui/dialog/object-attributes.cpp
+++ src/ui/dialog/object-attributes.cpp
@@ -1,14 +1,23 @@
 /** @file
  * Generic object attribute editor
  */
-/* Authors:
- *   Lauris Kaplinski <lauris@kaplinski.com>
- *   bulia byak <buliabyak@users.sf.net>
- *   Kris De Gussem <Kris.DeGussem@gmail.com>
- *
- * Copyright (C) 1999-2012 Authors
- *
- * Released under GNU GPL, read the file 'COPYING' for more information
+/* 
+ * Inkscape, an Open Source vector graphics editor
+ * Copyright (C) 2012 Kris De Gussem <Kris.DeGussem@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #include <glibmm/i18n.h>
--- src/ui/dialog/object-attributes.h
+++ src/ui/dialog/object-attributes.h
@@ -1,12 +1,23 @@
 /** @file
  * Generic object attribute editor
  */
-/* Author:
- *   Lauris Kaplinski <lauris@ximian.com>
- *
- * Copyright (C) 2001 Ximian, Inc.
- *
- * Licensed under GNU GPL
+/* 
+ * Inkscape, an Open Source vector graphics editor
+ * Copyright (C) 2012 Kris De Gussem <Kris.DeGussem@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #ifndef SEEN_DIALOGS_OBJECT_ATTRIBUTES_H
