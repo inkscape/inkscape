aa976ac02c19cc45a4250ff7614771d66b1ae609
Kris <Kris.De.Gussem@hotmail.com>
2012-09-09 21:31:12 +0200

    11658:   static code checking (fixes Bug #614227 )

--- src/libvpsc/csolve_VPSC.cpp
+++ src/libvpsc/csolve_VPSC.cpp
@@ -19,108 +19,109 @@
 using namespace vpsc;
 extern "C" {
 Variable* newVariable(int id, double desiredPos, double weight) {
-	return new Variable(id,desiredPos,weight);
+    return new Variable(id,desiredPos,weight);
 }
 Constraint* newConstraint(Variable* left, Variable* right, double gap) {
-	return new Constraint(left,right,gap);
+    return new Constraint(left,right,gap);
 }
 Solver* newSolver(int n, Variable* vs[], int m, Constraint* cs[]) {
-	return new Solver(n,vs,m,cs);
+    return new Solver(n,vs,m,cs);
 }
 Solver* newIncSolver(int n, Variable* vs[], int m, Constraint* cs[]) {
-	return (Solver*)new vpsc::IncSolver(n,vs,m,cs);
+    return (Solver*)new vpsc::IncSolver(n,vs,m,cs);
 }
 
 int genXConstraints(int n, boxf* bb, Variable** vs, Constraint*** cs,int transitiveClosure) {
-	Rectangle* rs[n];
-	for(int i=0;i<n;i++) {
-		rs[i]=new Rectangle(bb[i].LL.x,bb[i].UR.x,bb[i].LL.y,bb[i].UR.y);
-	}
-	int m = generateXConstraints(n,rs,vs,*cs,transitiveClosure);
-	for(int i=0;i<n;i++) {
-		delete rs[i];
-	}
-	return m;
+    Rectangle* rs[n];
+    for(int i=0;i<n;i++) {
+        rs[i]=new Rectangle(bb[i].LL.x,bb[i].UR.x,bb[i].LL.y,bb[i].UR.y);
+    }
+    int m = generateXConstraints(n,rs,vs,*cs,transitiveClosure);
+    for(int i=0;i<n;i++) {
+        delete rs[i];
+    }
+    return m;
 }
 int genYConstraints(int n, boxf* bb, Variable** vs, Constraint*** cs) {
-	Rectangle* rs[n];
-	for(int i=0;i<n;i++) {
-		rs[i]=new Rectangle(bb[i].LL.x,bb[i].UR.x,bb[i].LL.y,bb[i].UR.y);
-	}
-	int m = generateYConstraints(n,rs,vs,*cs);
-	for(int i=0;i<n;i++) {
-		delete rs[i];
-	}
-	return m;
+    assert(n > 0);
+    Rectangle* rs[n];
+    for(int i=0;i<n;i++) {
+        rs[i]=new Rectangle(bb[i].LL.x,bb[i].UR.x,bb[i].LL.y,bb[i].UR.y);
+    }
+    int m = generateYConstraints(n,rs,vs,*cs);
+    for(int i=0;i<n;i++) {
+        delete rs[i];
+    }
+    return m;
 }
 
 Constraint** newConstraints(int m) {
-	return new Constraint*[m];
+    return new Constraint*[m];
 }
 void deleteConstraints(int m, Constraint **cs) {
-	for(int i=0;i<m;i++) {
-		delete cs[i];
-	}
-	delete [] cs;
+    for(int i=0;i<m;i++) {
+        delete cs[i];
+    }
+    delete [] cs;
 }
 void deleteConstraint(Constraint* c) {
-	delete c;
+    delete c;
 }
 void deleteVariable(Variable* v) {
-	delete v;
+    delete v;
 }
 void satisfyVPSC(Solver* vpsc) {
-	try {
-		vpsc->satisfy();
-	} catch(const char *e) {
-		std::cerr << e << std::endl;
-		exit(1);
-	}
+    try {
+        vpsc->satisfy();
+    } catch(const char *e) {
+        std::cerr << e << std::endl;
+        exit(1);
+    }
 }
 int getSplitCnt(IncSolver *vpsc) {
-	return vpsc->splitCnt;
+    return vpsc->splitCnt;
 }
 void deleteVPSC(Solver *vpsc) {
-	assert(vpsc!=NULL);
-	delete vpsc;
+    assert(vpsc!=NULL);
+    delete vpsc;
 }
 void solveVPSC(Solver* vpsc) {
-	vpsc->solve();
+    vpsc->solve();
 }
 void splitIncVPSC(IncSolver* vpsc) {
-	vpsc->splitBlocks();
+    vpsc->splitBlocks();
 }
 void setVariableDesiredPos(Variable *v, double desiredPos) {
-	v->desiredPosition = desiredPos;
+    v->desiredPosition = desiredPos;
 }
 double getVariablePos(Variable *v) {
-	return v->position();
+    return v->position();
 }
 void remapInConstraints(Variable *u, Variable *v, double dgap) {
-	for(Constraints::iterator i=u->in.begin();i!=u->in.end();i++) {
-		Constraint* c=*i;	
-		c->right=v;
-		c->gap+=dgap;
-		v->in.push_back(c);
-	}
-	u->in.clear();
+    for(Constraints::iterator i=u->in.begin();i!=u->in.end();i++) {
+        Constraint* c=*i;    
+        c->right=v;
+        c->gap+=dgap;
+        v->in.push_back(c);
+    }
+    u->in.clear();
 }
 void remapOutConstraints(Variable *u, Variable *v, double dgap) {
-	for(Constraints::iterator i=u->out.begin();i!=u->out.end();i++) {
-		Constraint* c=*i;	
-		c->left=v;
-		c->gap+=dgap;
-		v->out.push_back(c);
-	}
-	u->out.clear();
+    for(Constraints::iterator i=u->out.begin();i!=u->out.end();i++) {
+        Constraint* c=*i;    
+        c->left=v;
+        c->gap+=dgap;
+        v->out.push_back(c);
+    }
+    u->out.clear();
 }
 int getLeftVarID(Constraint *c) {
-	return c->left->id;
+    return c->left->id;
 }
 int getRightVarID(Constraint *c){
-	return c->right->id;
+    return c->right->id;
 }
 double getSeparation(Constraint *c){
-	return c->gap;
+    return c->gap;
 }
 }
