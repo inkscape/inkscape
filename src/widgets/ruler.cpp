3a70ed451b69f301db0c47eb624fd305084c28e9
Alex Valavanis <valavanisalex@gmail.com>
2012-12-22 13:09:26 +0000

    11966:   ruler: Add modeline support

--- src/widgets/ruler.cpp
+++ src/widgets/ruler.cpp
@@ -927,3 +927,14 @@ void sp_ruler_set_metric(SPRuler *ruler, SPMetric metric)
 
   sp_ruler_invalidate_ticks(ruler);
 }
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99 :
