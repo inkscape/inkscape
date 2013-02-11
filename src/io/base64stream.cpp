66f8a229f709368e466fc48e8dff762c40e1e5c5
Kris <Kris.De.Gussem@hotmail.com>
2013-02-11 19:32:33 +0100

    12118:   Dropped duplicate code for dom/io and io/ (Bug #1120585 )

--- src/io/base64stream.cpp
+++ src/io/base64stream.cpp
@@ -5,15 +5,32 @@
  * of Base64 data with a stream interface, hiding
  * the implementation from the user.
  *
+ * http://www.w3.org/TR/2004/REC-DOM-Level-3-Core-20040407/idl-definitions.html
+ *
  * Authors:
- *   Bob Jamison <rjamison@titan.com>
+
+ *   Bob Jamison
  *
- * Copyright (C) 2004 Inkscape.org
+ * Copyright (C) 2006 Bob Jamison
  *
- * Released under GNU GPL, read the file 'COPYING' for more information
+
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include "base64stream.h"
+
 
 
 
