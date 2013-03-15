574ab3f32225a010a8b7a35956c9629f62a6d5b0
Alex Valavanis <valavanisalex@gmail.com>
2013-03-15 00:44:05 +0000

    12207:   Fix table layout in fill and stroke dialog in Gtk+ 2

--- src/ui/dialog/fill-and-stroke.cpp
+++ src/ui/dialog/fill-and-stroke.cpp
@@ -132,14 +132,24 @@ void
 FillAndStroke::_layoutPageFill()
 {
     fillWdgt = manage(sp_fill_style_widget_new());
+
+#if WITH_GTKMM_3_0
     _page_fill->table().attach(*fillWdgt, 0, 0, 1, 1);
+#else
+    _page_fill->table().attach(*fillWdgt, 0, 1, 0, 1);
+#endif
 }
 
 void
 FillAndStroke::_layoutPageStrokePaint()
 {
     strokeWdgt = manage(sp_stroke_style_paint_widget_new());
+
+#if WITH_GTKMM_3_0
     _page_stroke_paint->table().attach(*strokeWdgt, 0, 0, 1, 1);
+#else
+    _page_stroke_paint->table().attach(*strokeWdgt, 0, 1, 0, 1);
+#endif
 }
 
 void
@@ -148,7 +158,12 @@ FillAndStroke::_layoutPageStrokeStyle()
     //Gtk::Widget *strokeStyleWdgt = manage(Glib::wrap(sp_stroke_style_line_widget_new()));
     //Gtk::Widget *strokeStyleWdgt = static_cast<Gtk::Widget *>(sp_stroke_style_line_widget_new());
     strokeStyleWdgt = sp_stroke_style_line_widget_new();
+
+#if WITH_GTKMM_3_0
     _page_stroke_style->table().attach(*strokeStyleWdgt, 0, 0, 1, 1);
+#else
+    _page_stroke_style->table().attach(*strokeStyleWdgt, 0, 1, 0, 1);
+#endif
 }
 
 void
