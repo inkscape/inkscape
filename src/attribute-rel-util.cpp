fa2e1476291d6d1f75cc933c6ff1c97106af9bd7
tavmjong-free <tavmjong@free.fr>
2013-08-15 21:13:44 +0200

    12481:   Prevent writing out empty style strings.

--- src/attribute-rel-util.cpp
+++ src/attribute-rel-util.cpp
@@ -141,7 +141,11 @@ void sp_attribute_clean_style(Node *repr, unsigned int flags) {
   // sp_repr_css_set( repr, css, "style"); // Don't use as it will cause loop.
   Glib::ustring value;
   sp_repr_css_write_string(css, value);
-  repr->setAttribute("style", value.c_str());
+  if( value.empty() ) {
+      repr->setAttribute("style", NULL );
+  } else {
+      repr->setAttribute("style", value.c_str());
+  }
 
   sp_repr_css_attr_unref( css );
 }
