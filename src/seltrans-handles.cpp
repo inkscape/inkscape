3992110492fa85da93ba0898c4e873b16ca6de57
Johan B. C. Engelen <j.b.c.engelen@alumnus.utwente.nl>
2014-02-24 21:01:23 +0100

    13054:   Set the order of resize handles back to what they were before r12400. Now it's possible again to access all handles for horizontal lines + geometric bbox

--- src/seltrans-handles.cpp
+++ src/seltrans-handles.cpp
@@ -17,22 +17,22 @@ SPSelTransHandle const hands[] = {
 //center handle will be 0 so we can reference it quickly.
  {HANDLE_CENTER,  SP_ANCHOR_CENTER, GDK_CROSSHAIR,           12,      0.5, 0.5},
 //handle-type     anchor-nudge      cursor                   image    x    y
- {HANDLE_STRETCH, SP_ANCHOR_S,      GDK_TOP_SIDE,            3,       0.5, 1},
- {HANDLE_STRETCH, SP_ANCHOR_W,      GDK_RIGHT_SIDE,          2,       1,   0.5},
- {HANDLE_STRETCH, SP_ANCHOR_N,      GDK_BOTTOM_SIDE,         3,       0.5, 0},
- {HANDLE_STRETCH, SP_ANCHOR_E,      GDK_LEFT_SIDE,           2,       0,   0.5},
  {HANDLE_SCALE,   SP_ANCHOR_SE,     GDK_TOP_LEFT_CORNER,     0,       0,   1},
+ {HANDLE_STRETCH, SP_ANCHOR_S,      GDK_TOP_SIDE,            3,       0.5, 1},
  {HANDLE_SCALE,   SP_ANCHOR_SW,     GDK_TOP_RIGHT_CORNER,    1,       1,   1},
+ {HANDLE_STRETCH, SP_ANCHOR_W,      GDK_RIGHT_SIDE,          2,       1,   0.5},
  {HANDLE_SCALE,   SP_ANCHOR_NW,     GDK_BOTTOM_RIGHT_CORNER, 0,       1,   0},
+ {HANDLE_STRETCH, SP_ANCHOR_N,      GDK_BOTTOM_SIDE,         3,       0.5, 0},
  {HANDLE_SCALE,   SP_ANCHOR_NE,     GDK_BOTTOM_LEFT_CORNER,  1,       0,   0},
- {HANDLE_SKEW,    SP_ANCHOR_S,      GDK_SB_H_DOUBLE_ARROW,   8,       0.5, 1},
- {HANDLE_SKEW,    SP_ANCHOR_W,      GDK_SB_V_DOUBLE_ARROW,   9,       1,   0.5},
- {HANDLE_SKEW,    SP_ANCHOR_N,      GDK_SB_H_DOUBLE_ARROW,   10,      0.5, 0},
- {HANDLE_SKEW,    SP_ANCHOR_E,      GDK_SB_V_DOUBLE_ARROW,   11,      0,   0.5},
+ {HANDLE_STRETCH, SP_ANCHOR_E,      GDK_LEFT_SIDE,           2,       0,   0.5},
  {HANDLE_ROTATE,  SP_ANCHOR_SE,     GDK_EXCHANGE,            4,       0,   1},
+ {HANDLE_SKEW,    SP_ANCHOR_S,      GDK_SB_H_DOUBLE_ARROW,   8,       0.5, 1},
  {HANDLE_ROTATE,  SP_ANCHOR_SW,     GDK_EXCHANGE,            5,       1,   1},
+ {HANDLE_SKEW,    SP_ANCHOR_W,      GDK_SB_V_DOUBLE_ARROW,   9,       1,   0.5},
  {HANDLE_ROTATE,  SP_ANCHOR_NW,     GDK_EXCHANGE,            6,       1,   0},
+ {HANDLE_SKEW,    SP_ANCHOR_N,      GDK_SB_H_DOUBLE_ARROW,   10,      0.5, 0},
  {HANDLE_ROTATE,  SP_ANCHOR_NE,     GDK_EXCHANGE,            7,       0,   0},
+ {HANDLE_SKEW,    SP_ANCHOR_E,      GDK_SB_V_DOUBLE_ARROW,   11,      0,   0.5},
 };
 
 /*
