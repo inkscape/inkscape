7b45ce77fae4ed3e6a206230f0f80c3f8085c2b7
Johan B. C. Engelen <j.b.c.engelen@alumnus.utwente.nl>
2014-03-03 22:08:39 +0100

    13101:   - fix memleak   - fix { } usage   - delete NULL is guaranteed to be OK   - fix initialization (all paths will eventually init the var, but let the compiler figure that out, instead of a future programmer trying to locate a bug)

--- src/uri-references.cpp
+++ src/uri-references.cpp
@@ -71,13 +71,15 @@ void URIReference::attach(const URI &uri) throw(BadURIException)
     if(document && uri.getPath() && !skip ) {
         std::string base = document->getBase() ? document->getBase() : "";
         std::string path = uri.getFullPath(base);
-        if(!path.empty())
+        if(!path.empty()) {
             document = document->createChildDoc(path);
-        else
+        } else {
             document = NULL;
+        }
     }
     if(!document) {
         g_warning("Can't get document for referenced URI: %s", filename);
+        g_free( filename );
         return;
     }
     g_free( filename );
@@ -91,7 +93,7 @@ void URIReference::attach(const URI &uri) throw(BadURIException)
     /* for now this handles the minimal xpointer form that SVG 1.0
      * requires of us
      */
-    gchar *id;
+    gchar *id = NULL;
     if (!strncmp(fragment, "xpointer(", 9)) {
         /* FIXME !!! this is wasteful */
         /* FIXME: It looks as though this is including "))" in the id.  I suggest moving
@@ -113,9 +115,7 @@ void URIReference::attach(const URI &uri) throw(BadURIException)
 
     /* FIXME !!! validate id as an NCName somewhere */
 
-    if (_uri) {
-        delete _uri;
-    }
+    delete _uri;
     _uri = new URI(uri);
 
     _connection.disconnect();
