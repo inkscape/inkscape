ec7f6882707d2abd6e8a2085d07fc298e756c1ed
Martin Owens <doctormo@gmail.com>
2014-04-04 15:23:06 +0200

    13263:   Maximised should be the default state for new inkscape users

--- src/preferences-skeleton.h
+++ src/preferences-skeleton.h
@@ -293,6 +293,7 @@ static char const preferences_skeleton[] =
 #ifdef WIN32 // FIXME: Temporary Win32 special code to enable transient dialogs
 "    <group id=\"dialogsontopwin32\" value=\"0\"/>\n"
 #endif
+"    <group id=\"defaultwindowsize\" value=\"2\" />\n"
 "    <group id=\"arenatilescachesize\" value=\"8192\"/>\n"
 "    <group id=\"preservetransform\" value=\"0\"/>\n"
 "    <group id=\"clonecompensation\" value=\"1\"/>\n"
