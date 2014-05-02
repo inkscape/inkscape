3172300aa1d204981a3debef63acd48da19aa5b2
JazzyNico <nicoduf@yahoo.fr>
2014-05-02 14:09:23 +0200

    13331:   Removing the old SP_VERB_FILE_EXPORT (replaced with SP_VERB_DIALOG_EXPORT).

--- src/verbs.h
+++ src/verbs.h
@@ -52,7 +52,7 @@ enum {
     SP_VERB_FILE_PRINT,
     SP_VERB_FILE_VACUUM,
     SP_VERB_FILE_IMPORT,
-    SP_VERB_FILE_EXPORT,
+//    SP_VERB_FILE_EXPORT,
     SP_VERB_FILE_IMPORT_FROM_OCAL, /**< Import the file from Open Clip Art Library */
 //    SP_VERB_FILE_EXPORT_TO_OCAL, /**< Export the file to Open Clip Art  Library */
     SP_VERB_FILE_NEXT_DESKTOP,
