af51ae7fb2d49644245e319bed9c49aa9cdd26c0
bryce <bryce@ubuntu.com>
2014-05-17 10:50:05 -0700

    13390:   extensions:  Update tests for new barcode class names
      The barcode class names appear to have been TitleCased.

--- share/extensions/test/render_barcode.test.py
+++ share/extensions/test/render_barcode.test.py
@@ -10,15 +10,15 @@ import random
 import unittest
 
 # Allow import of the extension code and modules
-sys.path.append('..') 
+sys.path.append('..')
 
 from render_barcode import *
 
-import Barcode.EAN5
-import Barcode.EAN8
-import Barcode.EAN13
-import Barcode.UPCA
-import Barcode.UPCE
+import Barcode.Ean5
+import Barcode.Ean8
+import Barcode.Ean13
+import Barcode.Upca
+import Barcode.Upce
 
 digits = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 
@@ -43,23 +43,23 @@ class InsertBarcodeBasicTest(unittest.TestCase):
 
   def test_render_barcode_ian5(self):
     """Barcode IAN5"""
-    self.barcode_test( 'ean5', Barcode.EAN5 )
+    self.barcode_test( 'ean5', Barcode.Ean5 )
 
   def test_render_barcode_ian8(self):
     """Barcode IAN5"""
-    self.barcode_test( 'ean8', Barcode.EAN8 )
+    self.barcode_test( 'ean8', Barcode.Ean8 )
 
   def test_render_barcode_ian13(self):
     """Barcode IAN5"""
-    self.barcode_test( 'ean13', Barcode.EAN13 )
+    self.barcode_test( 'ean13', Barcode.Ean13 )
 
   def test_render_barcode_upca(self):
     """Barcode IAN5"""
-    self.barcode_test( 'upca', Barcode.UPCA )
+    self.barcode_test( 'upca', Barcode.Upca )
 
   def test_render_barcode_upce(self):
     """Barcode UPCE"""
-    self.barcode_test( 'upce', Barcode.UPCE )
+    self.barcode_test( 'upce', Barcode.Upce )
 
   def barcode_test(self, name, module):
     """Base module for all barcode testing"""
