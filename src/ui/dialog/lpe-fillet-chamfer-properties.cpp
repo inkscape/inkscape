6cb368568e03f4e6671453e0687f8c94d636aad5
Jon A. Cruz <jon@joncruz.org>
2014-11-10 09:04:48 -0800

    13699:   Fix 32-bit build break.

--- src/ui/dialog/lpe-fillet-chamfer-properties.cpp
+++ src/ui/dialog/lpe-fillet-chamfer-properties.cpp
@@ -64,7 +64,7 @@ FilletChamferPropertiesDialog::FilletChamferPropertiesDialog()
     _fillet_chamfer_chamfer_subdivisions.set_digits(0);
     _fillet_chamfer_chamfer_subdivisions.set_increments(1,1);
     //todo: get tha max aloable infinity freeze the widget
-    _fillet_chamfer_chamfer_subdivisions.set_range(0, 999999999999999999);
+    _fillet_chamfer_chamfer_subdivisions.set_range(0, 999999999999999999.0);
     
     _fillet_chamfer_chamfer_subdivisions_label.set_label(_("Chamfer subdivisions:"));
     _fillet_chamfer_chamfer_subdivisions_label.set_alignment(1.0, 0.5);
