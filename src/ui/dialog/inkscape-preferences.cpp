de61f8bf64aa2878ebbf59ccbaebb93b3a9d2a3c
Josh Andler <scislac@gmail.com>
2014-11-25 12:29:16 -0800

    13760:   Disable preference widget to allow saving fonts in non-px units

--- src/ui/dialog/inkscape-preferences.cpp
+++ src/ui/dialog/inkscape-preferences.cpp
@@ -461,7 +461,7 @@ void InkscapePreferences::initPageTools()
     _page_text.add_line( false, _("Text size unit type:"), _font_unit_type, "",
                        _("Set the type of unit used in the text toolbar and text dialogs"), false);
     _font_output_px.init ( _("Always output text size in pixels (px)"), "/options/font/textOutputPx", true);
-    _page_text.add_line( false, "", _font_output_px, "", _("Always convert the text size units above into pixels (px) before saving to file"));
+//    _page_text.add_line( false, "", _font_output_px, "", _("Always convert the text size units above into pixels (px) before saving to file"));
 
     this->AddNewObjectsStyle(_page_text, "/tools/text");
 
