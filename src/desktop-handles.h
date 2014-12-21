fc6f3ea40b81ab0aa7f033e42947348c7254f04a
Liam P. White <inkscapebrony@gmail.com>
2014-12-21 16:02:05 -0500

    13818:   Purge sp_desktop_guides

--- src/desktop-handles.h
+++ src/desktop-handles.h
@@ -45,7 +45,6 @@ namespace Inkscape {
 #define SP_COORDINATES_UNDERLINE_Y (1 << Geom::Y)
 
 SPDocument * sp_desktop_document (SPDesktop const * desktop);
-SPCanvasGroup * sp_desktop_guides (SPDesktop const * desktop);
 SPCanvasGroup * sp_desktop_controls (SPDesktop const * desktop);
 
 #endif // SEEN_SP_DESKTOP_HANDLES_H
