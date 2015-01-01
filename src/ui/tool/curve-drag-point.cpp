cb883712f1fe98fb42ddff34ec7eae4e6f314eb5
Jabiertxof <jtx@jtx.marker.es>
2015-01-01 10:10:01 +0100

    13830:   Allow reposition of BSpline using the path

--- src/ui/tool/curve-drag-point.cpp
+++ src/ui/tool/curve-drag-point.cpp
@@ -94,8 +94,18 @@ void CurveDragPoint::dragged(Geom::Point &new_pos, GdkEventMotion *event)
     if(!_pm.isBSpline(false)){
         first->front()->move(first->front()->position() + offset0);
         second->back()->move(second->back()->position() + offset1);
-    }else if(weight>=0.8 && held_shift(*event))second->back()->move(new_pos);
-    else if(weight<=0.2 && held_shift(*event))first->front()->move(new_pos);
+    }else if(weight>=0.8 && held_shift(*event)){
+        second->back()->move(new_pos);
+    }else if(weight<=0.2 && held_shift(*event)){
+        first->front()->move(new_pos);
+    }else if(weight>=0.7){
+        second->move(second->position() + delta);
+    }else if(weight<=0.3){
+        first->move(first->position() + delta);
+    }else{
+        first->move(first->position() + delta);
+        second->move(second->position() + delta);
+    }
     _pm.update();
 }
 
