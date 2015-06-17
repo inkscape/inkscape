bd3108899f9837244e8c34baa5f4bb6ee2c08812
William Entriken <github.com@phor.net>
2015-06-17 14:12:50 -0400

    Now include log messages

--- update.bash
+++ update.bash
@@ -3,8 +3,9 @@
 echo UPDATING
 (cd inkscape-launchpad; bzr update)
 REVNO=$(cd inkscape-launchpad; bzr revno)
+LOG=$(cd inkscape-launchpad; bzr log -r-1 --line)
 
 echo PULLED REVISION $REVNO
-git commit -am "Pull revision $REVNO"
+git commit -am "$LOG"
 
 echo GIT COMMITTED, READY TO git push
