6e8559f043253c7c0cf4a3bc7a631dee7151b6e9
~suv <suv-sf@users.sourceforge.net>
2015-09-22 18:12:36 +0200

    14382:   Extensions. Fix some broken Vim modelines

--- share/extensions/restack.py
+++ share/extensions/restack.py
@@ -151,4 +151,4 @@ if __name__ == '__main__':
     e.affect()
 
 
-# vim: expandtab shiftwidth=4 tabstop=8 softtabstop=4 encoding=utf-8 textwidth=99
+# vim: expandtab shiftwidth=4 tabstop=8 softtabstop=4 fileencoding=utf-8 textwidth=99
