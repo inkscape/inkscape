f0bfefb4137d995a12e8fa61576a4d030f4f3b71
Marc Jeanmougin <marcjeanmougin@free.fr>
2015-10-15 01:13:10 +0200

    14413:   clang-format

--- src/uri-references.cpp
+++ src/uri-references.cpp
@@ -26,71 +26,88 @@
 namespace Inkscape {
 
 URIReference::URIReference(SPObject *owner)
-    : _owner(owner), _owner_document(NULL), _obj(NULL), _uri(NULL)
+    : _owner(owner)
+    , _owner_document(NULL)
+    , _obj(NULL)
+    , _uri(NULL)
 {
     g_assert(_owner != NULL);
     /* FIXME !!! attach to owner's destroy signal to clean up in case */
 }
 
 URIReference::URIReference(SPDocument *owner_document)
-    : _owner(NULL), _owner_document(owner_document), _obj(NULL), _uri(NULL)
+    : _owner(NULL)
+    , _owner_document(owner_document)
+    , _obj(NULL)
+    , _uri(NULL)
 {
     g_assert(_owner_document != NULL);
 }
 
-URIReference::~URIReference()
-{
-    detach();
-}
+URIReference::~URIReference() { detach(); }
 
 /*
  * The main ideas here are:
  * (1) "If we are inside a clone, then we can accept if and only if our "original thing" can accept the reference"
- * (this caused problems when there are clones because a change in ids triggers signals for the object hrefing this id, but also its cloned reprs
- * (descendants of <use> referencing an ancestor of the href'ing object)). The way it is done here is *atrocious*, but i could not find a better way.
+ * (this caused problems when there are clones because a change in ids triggers signals for the object hrefing this id,
+ *but also its cloned reprs
+ * (descendants of <use> referencing an ancestor of the href'ing object)). The way it is done here is *atrocious*, but i
+ *could not find a better way.
  * FIXME: find a better and safer way to find the "original object" of anyone with the flag ->cloned
  *
- * (2) Once we have an (potential owner) object, it can accept a href to obj, iff the graph of objects where directed edges are
+ * (2) Once we have an (potential owner) object, it can accept a href to obj, iff the graph of objects where directed
+ *edges are
  * either parent->child relations , *** or href'ing to href'ed *** relations, stays acyclic.
- * We can go either from owner and up in the tree, or from obj and down, in either case this will be in the worst case linear in the number of objects.
- * There are no easy objects allowing to do the second proposition, while "hrefList" is a "list of objects href'ing us", so we'll take this.
+ * We can go either from owner and up in the tree, or from obj and down, in either case this will be in the worst case
+ *linear in the number of objects.
+ * There are no easy objects allowing to do the second proposition, while "hrefList" is a "list of objects href'ing us",
+ *so we'll take this.
  * Then we keep a set of already visited elements, and do a DFS on this graph. if we find obj, then BOOM.
  */
 
-bool URIReference::_acceptObject(SPObject *obj) const {
-	//we go back following hrefList and parent to find if the object already references ourselves indirectly
-	std::set<SPObject*> done;
-	SPObject * owner = getOwner();
-	if(!owner)return true;
-	while(owner->cloned){
-		std::vector<int> positions;
-        while(owner->cloned){
-        	int position=0;
-        	SPObject* c = owner->parent->firstChild();
-        	while(c != owner && dynamic_cast<SPObject*>(c) ){position++;c=c->next;}
-	    	positions.push_back(position);
-    		owner=owner->parent;
-    	}
-		    owner = ((SPUse*)owner)->get_original();
-		    for(int i=positions.size()-2;i>=0;i--)owner=owner->childList(false)[positions[i]];
-	}
-	//once we have the "original" object (hopefully) we look at who is referencing it
-        if(obj == owner)return false;
-	std::list<SPObject*> todo(owner->hrefList);
-	todo.push_front(owner->parent);
-	while(!todo.empty()){
-	    SPObject* e = todo.front();
-	    todo.pop_front();
-	    if(!dynamic_cast<SPObject*>(e))continue;
-	    if(done.insert(e).second){
-	    	if(e==obj){return false;}
-	    	todo.push_front(e->parent);
-	    	todo.insert(todo.begin(),e->hrefList.begin(),e->hrefList.end());
-	    }
-	}
+bool URIReference::_acceptObject(SPObject *obj) const
+{
+    // we go back following hrefList and parent to find if the object already references ourselves indirectly
+    std::set<SPObject *> done;
+    SPObject *owner = getOwner();
+    if (!owner)
+        return true;
+    while (owner->cloned) {
+        std::vector<int> positions;
+        while (owner->cloned) {
+            int position = 0;
+            SPObject *c = owner->parent->firstChild();
+            while (c != owner && dynamic_cast<SPObject *>(c)) {
+                position++;
+                c = c->next;
+            }
+            positions.push_back(position);
+            owner = owner->parent;
+        }
+        owner = ((SPUse *)owner)->get_original();
+        for (int i = positions.size() - 2; i >= 0; i--)
+            owner = owner->childList(false)[positions[i]];
+    }
+    // once we have the "original" object (hopefully) we look at who is referencing it
+    if (obj == owner)
+        return false;
+    std::list<SPObject *> todo(owner->hrefList);
+    todo.push_front(owner->parent);
+    while (!todo.empty()) {
+        SPObject *e = todo.front();
+        todo.pop_front();
+        if (!dynamic_cast<SPObject *>(e))
+            continue;
+        if (done.insert(e).second) {
+            if (e == obj) {
+                return false;
+            }
+            todo.push_front(e->parent);
+            todo.insert(todo.begin(), e->hrefList.begin(), e->hrefList.end());
+        }
+    }
     return true;
 }
-
 
 
 
@@ -109,32 +126,30 @@ void URIReference::attach(const URI &uri) throw(BadURIException)
     // PNG and JPG files are allowed (in the case of feImage).
     gchar *filename = uri.toString();
     bool skip = false;
-    if( g_str_has_suffix( filename, ".jpg" ) ||
-        g_str_has_suffix( filename, ".JPG" ) ||
-        g_str_has_suffix( filename, ".png" ) ||
-        g_str_has_suffix( filename, ".PNG" ) ) {
+    if (g_str_has_suffix(filename, ".jpg") || g_str_has_suffix(filename, ".JPG") ||
+        g_str_has_suffix(filename, ".png") || g_str_has_suffix(filename, ".PNG")) {
         skip = true;
     }
- 
+
     // The path contains references to separate document files to load.
-    if(document && uri.getPath() && !skip ) {
+    if (document && uri.getPath() && !skip) {
         std::string base = document->getBase() ? document->getBase() : "";
         std::string path = uri.getFullPath(base);
-        if(!path.empty()) {
+        if (!path.empty()) {
             document = document->createChildDoc(path);
         } else {
             document = NULL;
         }
     }
-    if(!document) {
+    if (!document) {
         g_warning("Can't get document for referenced URI: %s", filename);
-        g_free( filename );
+        g_free(filename);
         return;
     }
-    g_free( filename );
+    g_free(filename);
 
     gchar const *fragment = uri.getFragment();
-    if ( !uri.isRelative() || uri.getQuery() || !fragment ) {
+    if (!uri.isRelative() || uri.getQuery() || !fragment) {
         throw UnsupportedURIException();
     }
 
@@ -149,9 +164,9 @@ void URIReference::attach(const URI &uri) throw(BadURIException)
            the strlen calculation and validity testing to before strdup, and copying just
            the id without the "))".  -- pjrm */
         if (!strncmp(fragment, "xpointer(id(", 12)) {
-            id = g_strdup(fragment+12);
+            id = g_strdup(fragment + 12);
             size_t const len = strlen(id);
-            if ( len < 3 || strcmp(id+len-2, "))") ) {
+            if (len < 3 || strcmp(id + len - 2, "))")) {
                 g_free(id);
                 throw MalformedURIException();
             }
@@ -184,13 +199,14 @@ void URIReference::detach()
 
 void URIReference::_setObject(SPObject *obj)
 {
-    if ( obj && !_acceptObject(obj) ) {
+    if (obj && !_acceptObject(obj)) {
         obj = NULL;
     }
 
-    if ( obj == _obj ) return;
+    if (obj == _obj)
+        return;
 
-    SPObject *old_obj=_obj;
+    SPObject *old_obj = _obj;
     _obj = obj;
 
     _release_connection.disconnect();
@@ -211,7 +227,7 @@ void URIReference::_setObject(SPObject *obj)
  */
 void URIReference::_release(SPObject *obj)
 {
-    g_assert( _obj == obj );
+    g_assert(_obj == obj);
     _setObject(NULL);
 }
 
@@ -219,28 +235,27 @@ void URIReference::_release(SPObject *obj)
 
 
 
-SPObject* sp_css_uri_reference_resolve( SPDocument *document, const gchar *uri )
+SPObject *sp_css_uri_reference_resolve(SPDocument *document, const gchar *uri)
 {
-    SPObject* ref = NULL;
+    SPObject *ref = NULL;
 
-    if ( document && uri && ( strncmp(uri, "url(", 4) == 0 ) ) {
-        gchar *trimmed = extract_uri( uri );
-        if ( trimmed ) {
-            ref = sp_uri_reference_resolve( document, trimmed );
-            g_free( trimmed );
+    if (document && uri && (strncmp(uri, "url(", 4) == 0)) {
+        gchar *trimmed = extract_uri(uri);
+        if (trimmed) {
+            ref = sp_uri_reference_resolve(document, trimmed);
+            g_free(trimmed);
         }
     }
 
     return ref;
 }
 
-SPObject *
-sp_uri_reference_resolve (SPDocument *document, const gchar *uri)
+SPObject *sp_uri_reference_resolve(SPDocument *document, const gchar *uri)
 {
-    SPObject* ref = NULL;
+    SPObject *ref = NULL;
 
-    if ( uri && (*uri == '#') ) {
-        ref = document->getObjectById( uri + 1 );
+    if (uri && (*uri == '#')) {
+        ref = document->getObjectById(uri + 1);
     }
 
     return ref;
