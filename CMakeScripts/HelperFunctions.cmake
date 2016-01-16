4dd6ebbc0d083192625bc2aa5744acc8a1091d27
bryce <bryce@bryceharrington.org>
2016-01-15 23:54:47 -0800

    14588:   cmake: Move join() to helper functions module

--- CMakeScripts/HelperFunctions.cmake
+++ CMakeScripts/HelperFunctions.cmake
@@ -17,3 +17,18 @@ function(pkg_check_variable _pkg _name)
 
     set("${_output_name}" "${_pkg_result}" CACHE STRING "pkg-config variable ${_name} of ${_pkg}")
 endfunction()
+
+# Join a cmake list of strings with a given glue character/string
+# E.g. join(MY_RESULT, ",", "1; 2; 3;") returns "1, 2, 3"
+function(join OUTPUT GLUE)
+    set(_TMP_RESULT "")
+    set(_GLUE "") # effective glue is empty at the beginning
+    foreach(arg ${ARGN})
+	# Skip empty lines
+	if(NOT arg STREQUAL "\n")
+	    set(_TMP_RESULT "${_TMP_RESULT}${_GLUE}${arg}")
+	    set(_GLUE "${GLUE}")
+	endif()
+    endforeach()
+    set(${OUTPUT} "${_TMP_RESULT}" PARENT_SCOPE)
+endfunction()
