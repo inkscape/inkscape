f43505d985dfa267cc721764da64837216792c4e
bryce <bryce@bryceharrington.org>
2016-01-17 13:02:42 -0800

    14606:   cmake: Fix installation directories for man pages
      We're not (yet) using the GnuInstallDirs module, so we don't have things
      like CMAKE_INSTALL_MANDIR and such defined.

--- CMakeScripts/Pod2man.cmake
+++ CMakeScripts/Pod2man.cmake
@@ -48,7 +48,7 @@ macro(pod2man PODFILE_FULL RELEASE SECTION CENTER)
         )
         install(
             FILES ${MANFILE_FULL}
-            DESTINATION ${CMAKE_INSTALL_MANDIR}/man${SECTION}
+            DESTINATION ${CMAKE_INSTALL_PREFIX}/${SHARE_INSTALL}/man/man${SECTION}
         )
     endif()
 endmacro(pod2man PODFILE NAME SECTION CENTER)
