b2a7f067456d8e3baf9593189db08377f3541b86
JazzyNico <nicoduf@yahoo.fr>
2016-05-24 15:23:46 +0200

    14912:   [Bug #1071833] major update of Jitter Nodes extension.

--- share/extensions/test/jitternodes.test.py
+++ share/extensions/test/jitternodes.test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# This is only the automatic generated test file for ../radiusrand.py
+# This is only the automatic generated test file for ../jitternodes.py
 # This must be filled with real tests and this commentary
 # must be cleared.
 # If you want to help, read the python unittest documentation:
@@ -10,15 +10,15 @@ import sys
 sys.path.append('..') # this line allows to import the extension code
 
 import unittest
-from radiusrand import *
+from jitternodes import *
 
-class RadiusRandomizeBasicTest(unittest.TestCase):
+class JitterNodesBasicTest(unittest.TestCase):
 
   #def setUp(self):
 
   def test_run_without_parameters(self):
     args = [ 'minimal-blank.svg' ]
-    e = RadiusRandomize()
+    e = JitterNodes()
     e.affect( args, False )
     #self.assertEqual( e.something, 'some value', 'A commentary about that.' )
 
