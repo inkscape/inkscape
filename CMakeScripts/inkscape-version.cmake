1d0bc990e687a51e0bbd013df5ef298d76225cc5
Marc Jeanmougin <marcjeanmougin@free.fr>
2016-11-07 23:36:49 +0100

    15223:   annotate custom builds, and add correct revno into make dist tarballs

--- CMakeScripts/inkscape-version.cmake
+++ CMakeScripts/inkscape-version.cmake
@@ -7,13 +7,23 @@
 
 # We should extract the version from build.xml
 # but for now just hard code
+
 set(INKSCAPE_REVISION "unknown")
+set(INKSCAPE_CUSTOM "custom")
 
 if(EXISTS ${INKSCAPE_SOURCE_DIR}/.bzr/)
     execute_process(COMMAND
 	bzr revno --tree ${INKSCAPE_SOURCE_DIR}
 	OUTPUT_VARIABLE INKSCAPE_REVISION
 	OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    execute_process(COMMAND
+        bzr status -S -V ${INKSCAPE_SOURCE_DIR}/src
+        OUTPUT_VARIABLE INKSCAPE_SOURCE_MODIFIED
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(NOT INKSCAPE_SOURCE_MODIFIED STREQUAL "")
+    string(CONCAT INKSCAPE_REVISION ${INKSCAPE_REVISION} " " ${INKSCAPE_CUSTOM})
+    endif()
 endif()
 message("revision is " ${INKSCAPE_REVISION})
 
